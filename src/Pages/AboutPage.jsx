a8bc781b0509415d462ce4786f74def3421293e6
miqdad bada <mujaidbada@gmail.com>
2025-04-28 21:10:35 +0100

    meet-boss

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -76,10 +76,9 @@ const AboutPage = () => {
                </div>
                <div className='Proces-hero-box'>
                   <h1 className='number-proces'>02.</h1>
-                  <h1 className='discover'> Discover</h1>
-                  <p className='p-process'> We begin by understanding your brand,
-                      goals, audience, and challenges. This
-                      phase involves deep research and audits to uncover insights that inform strategy.
+                  <h1 className='discover'> Define </h1>
+                  <p className='p-process'> We clearly outline your digital marketing objectives, success 
+                    metrics, and strategic direction—ensuring alignment with your business vision.
                   </p>
                </div>
              </div>
@@ -87,36 +86,33 @@ const AboutPage = () => {
            <div className='process-hero-divs'>
                <div className='Proces-hero-box'>
                   <h1 className='number-proces'>03.</h1>
-                  <h1 className='discover'> Discover</h1>
-                  <p className='p-process'> We begin by understanding your brand,
-                      goals, audience, and challenges. This
-                      phase involves deep research and audits to uncover insights that inform strategy.
+                  <h1 className='discover'> Design </h1>
+                  <p className='p-process'> Our creative team brings ideas to life with compelling content, visuals,
+                     campaigns, and experiences tailored to your brand identity and target audience.
                   </p>
                </div>
                <div className='Proces-hero-box'>
                   <h1 className='number-proces'>04.</h1>
-                  <h1 className='discover'> Discover</h1>
-                  <p className='p-process'> We begin by understanding your brand,
-                      goals, audience, and challenges. This
-                      phase involves deep research and audits to uncover insights that inform strategy.
+                  <h1 className='discover'>  Develop  </h1>
+                  <p className='p-process'> We execute strategies using cutting-edge tools, platforms, and 
+                    technologies—whether it's building websites, setting up email funnels, or launching
+                     digital campaigns.
                   </p>
                </div>
              </div>
              <div className='process-hero-divs'>
                <div className='Proces-hero-box'>
                   <h1 className='number-proces'>05.</h1>
-                  <h1 className='discover'> Discover</h1>
-                  <p className='p-process'> We begin by understanding your brand,
-                      goals, audience, and challenges. This
-                      phase involves deep research and audits to uncover insights that inform strategy.
+                  <h1 className='discover'> Deploy  </h1>
+                  <p className='p-process'> With precision and timing, we launch campaigns and content across relevant 
+                    platforms—ensuring everything is optimized for visibility and engagement.
                   </p>
                </div>
                <div className='Proces-hero-box'>
                   <h1 className='number-proces'>06.</h1>
-                  <h1 className='discover'> Discover</h1>
-                  <p className='p-process'> We begin by understanding your brand,
-                      goals, audience, and challenges. This
-                      phase involves deep research and audits to uncover insights that inform strategy.
+                  <h1 className='discover'>  Drive  </h1>
+                  <p className='p-process'> Post-launch, we analyze results, optimize performance, and scale what works.
+                     We stay committed to continuous growth and long-term success.
                   </p>
                </div>
              </div>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,6 +4,10 @@ import NavBar from '../Components/NavBar'
 import Footer from '../Components/Footer';
 import AOS from 'aos';
 import 'aos/dist/aos.css'
+import { FaHome } from "react-icons/fa";
+import latifat from "../assets/Images/SML.jpg"
+import Nafisat from "../assets/Images/S.E.O.jpg"
+import Rodiyah from "../assets/Images/MANAGER.jpg"
 import CallTag from '../Components/CallTag';
 
 const HomePage = () => {
@@ -34,7 +38,91 @@ const HomePage = () => {
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit delectus recusandae ex. <br />
           Adipisci veritatis quis non enim sunt maxime eum!
         </p>
-      </section>
+      </section>    <div className='service-hero'>
+        
+        <div className='service-hero-divs'>
+          <div className='service-hero-box'>
+             <FaHome class="course-icon"/>
+             <h1>Social Media Marketing</h1>
+             <p>Social media marketing, SEO, PPC
+                campaigns, and email marketing to
+                enhance brand visibility, engagement, and
+                conversions</p>
+          </div>
+          <div className='service-hero-box'>
+             <FaHome class="course-icon"/>
+             <h1>Web Design & Development</h1>
+             <p>   Designing user-friendly, SEO-optimized
+                  websites that serve as the cornerstone of a
+                  business’s online presence.</p>
+          </div>
+        </div>
+
+        <div className='service-hero-divs'>
+          <div className='service-hero-box'>
+             <FaHome class="course-icon"/>
+             <h1>Brand Strategy and Identity:</h1>
+             <p>  Crafting unique brand messages, logos,
+                  and visual identities to differentiate clients
+                  from competitors.</p>
+          </div>
+          <div className='service-hero-box'>
+             <FaHome class="course-icon"/>
+             <h1>Content Marketing</h1>
+             <p>  Delivering high-quality blogs, articles,
+                  videos, and graphic designs to resonate
+                  with target audiences </p>
+          </div>
+          
+        </div>
+
+        <div className='service-hero-divs'>
+          <div className='service-hero-box'>
+             <FaHome class="course-icon"/>
+             <h1>Training and Consulting:</h1>
+             <p> Providing workshops, team training, and  
+              strategic advice to empower businesses
+              with the skills they need to succeed</p>
+          </div>
+          <div className='service-hero-box'>
+             <FaHome class="course-icon"/>
+             <h1> Community Management and Engagement</h1>
+             <p> Building and nurturing online
+                 communities to foster brand loyalty and
+                 trust</p>
+          </div>
+        </div>
+        
+      </div>
+
+
+      <div className='meet-the-team-section'>
+     <div className='Team-text'>
+      <h1>Meet Our Leadership</h1>
+      <p className='text-meet-team'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, adipisci ullam. Itaque
+         quasi quibusdam necessitatibus impedit eius blanditiis ut suscipit nobis tempora, .</p>
+     </div>
+     <div className='picture-divs'>
+           <div className='picture-box'>
+             <div className='team-pics' > </div>  
+              <h4>Miqdad Bada</h4>
+              <p className='team-position'>Founder</p>
+           </div>
+           <div className='picture-box'>
+            <div className='team-pics-2'> </div>  
+              <h4>Jamal Opeyemi</h4>
+              <p className='team-position'>S.E.O</p>
+           </div>
+           <div className='picture-box'>
+             <div className='team-pics-3'> </div> 
+              <h4>Muhammad Bada</h4>
+              <p className='team-position'>Manager</p>
+           </div>
+     </div>
+
+   </div>
+
+     
 
     </main>
      <CallTag />
